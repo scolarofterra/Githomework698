970fcc7462b6066a0c20e8df3080e17bc6acfa9e
scolarofterra <travisdeschenes@gmail.com>
2018-04-11 19:32:48 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,7 +10,7 @@ provider "google" {
 }
 
 resource "google_compute_instance_template" "tdd1007-template-server" {
-  name_prefix  = "tdd1007-template-server-"
+  name_prefix  = "tdd1007templateserver-"
   machine_type = "f1-micro"
   region       = "us-central1"
 
@@ -26,7 +26,7 @@ resource "google_compute_instance_template" "tdd1007-template-server" {
 
 resource "google_compute_instance_group_manager" "tdd1007-watcher-server" {
   name               = "tdd1007-watcher-server"
-  instance_template  = "${google_compute_instance_template.tdd1007-watcher-server.self_link}"
+  instance_template  = "${google_compute_instance_template.tdd1007-template-server.self_link}"
   base_instance_name = "tdd1007-watcher-server"
   zone               = "us-central1-a"
   target_size        = "1"
