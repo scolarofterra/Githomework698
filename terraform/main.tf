2d4e6bb88ec35b0dd12ff2a7b22d49b0b1698f1d
scolarofterra <travisdeschenes@gmail.com>
2018-04-18 19:02:06 -0400

    fix wrong chocies in a conflict resolve

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,17 +7,22 @@ terraform {
 }
 provider "google" {
   region = "us-central1"
+  project = "comp698-tdd1007"
 }
 
 resource "google_compute_instance_template" "tdd1007-template-server" {
-  name  = "tdd1007-template-server"
+  name_prefix  = "tdd1007templateserver-"
   machine_type = "f1-micro"
   region       = "us-central1"
 
   // boot disk
   disk {
-    source_image = "cos-stable"
-  }
+    source_image = "cos-cloud/cos-stable"
+-  }
+- 
+-  network_interface {
+-     network = "default"
+    }
 
   lifecycle {
     create_before_destroy = true
@@ -37,8 +42,8 @@ EOF
 
 resource "google_compute_instance_group_manager" "tdd1007-watcher-server" {
   name               = "tdd1007-watcher-server"
-  instance_template  = "${google_compute_instance_template.instance_template.self_link}"
-  base_instance_name = "tf-server"
+  instance_template  = "${google_compute_instance_template.tdd1007-template-server.self_link}"
+  base_instance_name = "tdd1007-watcher-server"
   zone               = "us-central1-a"
   target_size        = "1"
 }
