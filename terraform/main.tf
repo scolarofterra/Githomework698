301834b8bd0939a0ff1d2609311da5cc3f2fcd4f
scolarofterra <travisdeschenes@gmail.com>
2018-04-23 03:49:03 -0400

    add third server

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,8 +10,8 @@ provider "google" {
   project = "comp698-tdd1007"
 }
 
-resource "google_compute_instance_template" "tdd1007-template-server" {
-  name_prefix  = "tdd1007templateserver-"
+resource "google_compute_instance_template" "prod-run" {
+  name_prefix  = "prodrun-"
   machine_type = "f1-micro"
   region       = "us-central1"
   tags = ["http-server"]
@@ -54,14 +54,69 @@ EOF
 
 }
 
-resource "google_compute_instance_group_manager" "tdd1007-watcher-server" {
-  name               = "tdd1007-watcher-server"
-  instance_template  = "${google_compute_instance_template.tdd1007-template-server.self_link}"
-  base_instance_name = "tdd1007-watcher-server"
+resource "google_compute_instance_group_manager" "prod" {
+  name               = "prod"
+  instance_template  = "${google_compute_instance_template.prod-run.self_link}"
+  base_instance_name = "prod"
   zone               = "us-central1-a"
 
 
   target_size        = "2"
+
+}
+
+resource "google_compute_instance_template" "staging-run" {
+  name_prefix  = "stagingrun-"
+  machine_type = "f1-micro"
+  region       = "us-central1"
+  tags = ["http-server"]
+  service_account {
+    scopes = [
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/cloud-platform",
+      "https://www.googleapis.com/auth/devstorage.read_write",
+    ]
+  }
+
+
+  // boot disk
+  disk {
+    source_image = "cos-cloud/cos-stable"
+  }
+ 
+  network_interface {
+     network = "default"
+
+     access_config {
+          // Ephemeral IP
+    }
+    }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+  metadata {
+    gce-container-declaration = <<EOF
+spec:
+  containers:
+  - image: 'gcr.io/comp698-tdd1007/github-scolarofterra-githomework698:9eee7e0eec5aeda2193e5b0dcc14524ae2011889'
+    name: service-container
+    stdin: false
+    tty: false
+  restartPolicy: Always
+EOF
+  }
+
+}
+
+resource "google_compute_instance_group_manager" "staging" {
+  name               = "staging"
+  instance_template  = "${google_compute_instance_template.staging-run.self_link}"
+  base_instance_name = "staging"
+  zone               = "us-central1-a"
+
+
+  target_size        = "1"
 
 }
 
